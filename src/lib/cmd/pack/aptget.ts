59b10d114ee07f0edd9bd8846bb17a1f22c14e0f
Marshall Porter <meoporter@gmail.com>
2024-12-15 23:08:34 -0800

    cond sudo

--- src/lib/cmd/pack/aptget.ts
+++ src/lib/cmd/pack/aptget.ts
@@ -1,53 +1,65 @@
 import type { Pack } from '../pack.i.ts'
 
+import { isInPath } from '../../path.ts'
 import { filterShell, spawnShell } from '../../shell.ts'
 
 export class AptGet implements Pack {
-  program = 'sudo apt-get'
+  async getProgram(): Promise<string> {
+    return (await isInPath('sudo')) ? 'sudo apt-get' : 'apt-get'
+  }
 
   async add(options: { names: Array<string> }): Promise<void> {
-    await spawnShell(`${this.program} update`)
+    await spawnShell(`${await this.getProgram()} update`)
     const filter = ` ${options.names.join(' ')}`
-    await spawnShell(`${this.program} install` + filter)
+    await spawnShell(`${await this.getProgram()} install` + filter)
   }
   async del(options: { names: Array<string> }): Promise<void> {
     const filter = ` ${options.names.join(' ')}`
-    await spawnShell(`${this.program} purge` + filter)
-    await spawnShell(`${this.program} autoremove`)
+    await spawnShell(`${await this.getProgram()} purge` + filter)
+    await spawnShell(`${await this.getProgram()} autoremove`)
   }
   async find(options: { name: string }): Promise<void> {
-    await spawnShell(`${this.program} update`)
+    await spawnShell(`${await this.getProgram()} update`)
     const filter = ` ${options.name}`
     await spawnShell(
-      `${this.program.replace('apt-get', 'apt-cache')} search` + filter,
+      `${(await this.getProgram()).replace('apt-get', 'apt-cache')} search` +
+        filter,
     )
   }
   async list(options: { names: Array<string> | undefined }): Promise<void> {
-    await filterShell(`${this.program} list --installed`, options.names)
+    await filterShell(
+      `${await this.getProgram()} list --installed`,
+      options.names,
+    )
   }
   async out(options: { names: Array<string> | undefined }): Promise<void> {
-    await spawnShell(`${this.program} update`)
-    await filterShell(`${this.program} list --upgradeable`, options.names)
+    await spawnShell(`${await this.getProgram()} update`)
+    await filterShell(
+      `${await this.getProgram()} list --upgradeable`,
+      options.names,
+    )
   }
   async tidy(): Promise<void> {
-    await spawnShell(`${this.program} autoclean`)
+    await spawnShell(`${await this.getProgram()} autoclean`)
   }
   async up(
     options: { names: Array<string> | undefined },
     upgradeCmd: string = 'dist-upgrade',
   ): Promise<void> {
-    await spawnShell(`${this.program} update`)
+    await spawnShell(`${await this.getProgram()} update`)
     if ((options.names?.length ?? 0) > 0) {
       const filter = ` ${options.names!.join(' ')}`
-      await spawnShell(`${this.program} install` + filter)
+      await spawnShell(`${await this.getProgram()} install` + filter)
     } else {
-      await spawnShell(`${this.program} ${upgradeCmd}`)
+      await spawnShell(`${await this.getProgram()} ${upgradeCmd}`)
     }
   }
 }
 
 export class Apt extends AptGet {
-  program = 'sudo apt'
+  async getProgram(): Promise<string> {
+    return (await isInPath('sudo')) ? 'sudo apt' : 'apt'
+  }
 
   async up(options: { names: Array<string> | undefined }): Promise<void> {
     await super.up(options, 'full-upgrade')
