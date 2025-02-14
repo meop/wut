75386e31ed0058b0d488d7b3aa4ddba0cc191552
Marshall Porter <meoporter@gmail.com>
2025-02-13 16:20:30 -0800

    more termux

--- prep/termux.zsh
+++ prep/termux.zsh
@@ -30,15 +30,19 @@ zsh "${WUT_CONFIG_LOCATION}/bin/zsh/setup/fzf.zsh"
 zsh "${WUT_CONFIG_LOCATION}/bin/zsh/setup/nvim.zsh"
 zsh "${WUT_CONFIG_LOCATION}/bin/zsh/setup/tmux.zsh"
 
-mkdir -p "${HOME}/.ssh" > /dev/null 2>&1
-cp "${WUT_CONFIG_LOCATION}/dot/ssh/config" "${HOME}/.ssh/config"
-chmod u=rw,g=,o= "${HOME}/.ssh/config"
+rm -r -f "${HOME}/.zsh" > /dev/null 2>&1
 
 mkdir -p "${HOME}/.config" > /dev/null 2>&1
-cp "${WUT_CONFIG_LOCATION}/dot/starship/starship.toml" "${HOME}/.config/starship.toml"
+mkdir -p "${HOME}/.ssh" > /dev/null 2>&1
+mkdir -p "${HOME}/.zsh" > /dev/null 2>&1
 
+cp "${WUT_CONFIG_LOCATION}/dot/git/gitconfig" "${HOME}/.gitconfig"
+cp "${WUT_CONFIG_LOCATION}/dot/nvim/init.lua" "${HOME}/.config/nvim/init.lua"
+cp "${WUT_CONFIG_LOCATION}/dot/ssh/config" "${HOME}/.ssh/config"
+cp "${WUT_CONFIG_LOCATION}/dot/starship/starship.toml" "${HOME}/.config/starship.toml"
+cp "${WUT_CONFIG_LOCATION}/dot/tmux/tmux.conf" "${HOME}/.tmux.conf"
 cp "${WUT_CONFIG_LOCATION}/dot/zsh/zshrc" "${HOME}/.zshrc"
 cp "${WUT_CONFIG_LOCATION}/dot/zsh/zshenv" "${HOME}/.zshenv"
-rm -r -f "${HOME}/.zsh"
-mkdir -p "${HOME}/.zsh" > /dev/null 2>&1
 cp "${WUT_CONFIG_LOCATION}/dot/zsh/zsh/"* "${HOME}/.zsh"
+
+chmod u=rw,g=,o= "${HOME}/.ssh/config" > /dev/null 2>&1
