8099ee30f7bae501719fd24a390dbf26b486e221
Marshall Porter <meoporter@gmail.com>
2025-06-30 20:29:47 +0000

    move

--- src/cli/zsh/script/install/fonts.zsh
+++ src/cli/zsh/script/install/fonts.zsh
@@ -5,7 +5,7 @@ function () {
     if [[ $YES ]]; then
       yn='y'
     else
-      read 'yn?? setup nerd fonts (local) [y, [n]] '
+      read 'yn?? install nerd fonts (local) [y, [n]] '
     fi
     if [[ $yn != 'n' ]]; then
       local share="${XDG_DATA_HOME:-${HOME}/.local/share}"
